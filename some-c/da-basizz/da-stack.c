cd09540882dad5a7b9c8926062b8d464b0325462
phcquangg <quanggtangg@gmail.com>
2026-04-22 21:18:54 +0700

    c: update da-stack keep da pointer w malloc

--- some-c/da-basizz/da-stack.c
+++ some-c/da-basizz/da-stack.c
@@ -1,6 +1,6 @@
 // Write a function that returns a local variable by value vs by pointer. Try returning a pointer to a local variable — observe what happens. Research: stack frames, undefined behavior, why JS doesn't have this problem.
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int returnValue() {
     int num = 12;
@@ -8,13 +8,20 @@ int returnValue() {
     return num;
 }
 
-int* returnPointer() {
+int returnPointer() {
     int num = 12;
 
-    return &num;
+    return num;
 }
 
+int* returnHeap() {
+	int* num = malloc(sizeof(int));
+	*num = 12;
+	return num;	
+}
+
+
 int main() {
-    printf("%p", returnValue());
+    printf("%p", returnHeap());
     return 0;
 }
