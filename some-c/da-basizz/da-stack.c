c194806d306cf256401c798e45fa9135ffc40952
phcquangg <quanggtangg@gmail.com>
2026-04-22 20:22:01 +0700

    c: da stack and its stuff

---
+++ some-c/da-basizz/da-stack.c
@@ -0,0 +1,20 @@
+// Write a function that returns a local variable by value vs by pointer. Try returning a pointer to a local variable — observe what happens. Research: stack frames, undefined behavior, why JS doesn't have this problem.
+#include <stdio.h>
+
+
+int returnValue() {
+    int num = 12;
+
+    return num;
+}
+
+int* returnPointer() {
+    int num = 12;
+
+    return &num;
+}
+
+int main() {
+    printf("%p", returnValue());
+    return 0;
+}
